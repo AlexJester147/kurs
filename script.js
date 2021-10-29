e5b2331e2d4502685118d31bdc2fa9158f7d33d3
AlexJester147 <vardy2016@lenta.ru>
2021-10-29 18:02:17 +0300

    feat : add forms

--- script.js
+++ script.js
@@ -1,17 +1,98 @@
 const nextButton = document.querySelector('#next-button');
-const previousButton = document.querySelector('#previous-button');
+const backButton = document.querySelector('#back-button');
 const mainBlock = document.querySelector('.main-block');
 const dataPart = document.querySelector('.data-part');
 const formPart = document.querySelector('.form-part');
+const firstForm = document.querySelector('.first-form');
+const secondForm = document.querySelector('.second-form');
+
+const rowsInput = document.querySelector('.rows-input');
+const colsInput = document.querySelector('.cols-input');
+
+const formCaption = document.querySelectorAll('.form-caption');
+
+
+let select = `<select>
+<option>≥</option>
+<option>≤</option>
+<option>=</option>
+</select>`;
+
+let selectMax = `<select>
+<option>max</option>
+<option>min</option>
+</select>`;
+
+let col = `<td><input type="number" class="col" min="0" step="1" value="0"></td>`;
+let row = `<tr><input type="number" class="row" min="0" step="1" value="0"></tr>`;
+
 
 nextButton.addEventListener('click', () => {
-  previousButton.parentNode.style.marginRight = 0;
+
+  firstForm.innerHTML = '';
+  secondForm.innerHTML = '';
+
+  backButton.parentNode.style.marginRight = 0;
   nextButton.parentNode.style.marginLeft = '-' + (nextButton.parentElement.offsetWidth + 50) + 'px';
   mainBlock.style.width = formPart.offsetWidth + 'px';
+
+  let caption;
+
+  //подпись формы
+  for (let i = 0; i < colsInput.value; i++) {
+    caption = document.createElement('div');
+    caption.classList.add('form-caption');
+    caption.innerHTML+=`x${i}`;
+    firstForm.append(caption);
+  }
+
+//первая таблица
+  for (let i = 0; i < rowsInput.value; i++) {
+    let tr = document.createElement('tr');
+    tr.innerHTML = col;
+    
+    for (let i = 0; i < colsInput.value-1; i++) {
+      let td = document.createElement('td');
+      td.innerHTML = row;
+      tr.appendChild(td);
+    }
+      firstForm.appendChild(tr);
+  }
+
+  //вторая таблица
+
+  for (let i = 0; i < colsInput.value; i++) {
+    caption = document.createElement('div');
+    caption.classList.add('form-caption');
+    caption.innerHTML+=`x${i}`;
+    secondForm.append(caption);
+  }
+
+  for (let i = 0; i < 1; i++) {
+    let tr = document.createElement('tr');
+    tr.innerHTML = row;
+    
+    for (let i = 0; i < colsInput.value-1; i++) {
+      let td = document.createElement('td');
+      td.innerHTML = col;
+      tr.appendChild(td);
+    }
+      secondForm.appendChild(tr);
+  }
+    
+
+    let tdCollection = document.querySelector('.second-form tr');
+    tdCollection.innerHTML+=selectMax+col;
+
+  let trCollection = document.querySelectorAll('.first-form tr');
+  trCollection.forEach(item => item.innerHTML+=select+row);
+
 });
 
-previousButton.addEventListener('click', () => {
+
+backButton.addEventListener('click', () => {
   nextButton.parentNode.style.marginLeft = 0;
   mainBlock.style.width = dataPart.offsetWidth + 'px';
-  previousButton.parentNode.style.marginRight = '-' + (previousButton.parentElement.offsetWidth - 50) + 'px';
-})
+  backButton.parentNode.parentNode.style.marginRight = '-' + (backButton.parentElement.parentNode.offsetWidth - 50) + 'px';
+});
+
